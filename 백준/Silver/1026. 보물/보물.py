fca8ba4996319dfd01fefa0a2145fc51627b5beb
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2023-10-16 23:43:18 +0900

    [Silver IV] Title: 보물, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,13 @@
+# 보물
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = sorted(list(map(int, input().split())))
+b = sorted(list(map(int, input().split())), reverse=True)
+
+s = 0
+for i in range(n):
+    s += a[i] * b[i]
+
+print(s)
