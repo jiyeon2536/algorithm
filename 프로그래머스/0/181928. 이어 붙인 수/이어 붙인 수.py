906f2caff1c0752e7b2d1d02a9270143b0ae9892
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-06-12 10:47:07 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -1,9 +1,4 @@
 def solution(num_list):
-    odd = ''
-    even = ''
-    for n in num_list:
-        if n % 2: 
-            odd += str(n)
-        else:
-            even += str(n)
+    even = ''.join([str(n) for n in num_list if not n % 2])
+    odd = ''.join([str(n) for n in num_list if n % 2])
     return int(odd) + int(even)
