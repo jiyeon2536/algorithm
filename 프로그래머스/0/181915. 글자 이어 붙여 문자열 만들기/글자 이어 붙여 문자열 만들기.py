c99ae07b5651071cc592c35e24d7b86c26a075df
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-06-12 10:20:48 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.py
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.py
@@ -1,5 +1,2 @@
 def solution(my_string, index_list):
-    answer = ''
-    for i in index_list:
-        answer += my_string[i]
-    return answer
+    return ''.join([my_string[idx] for idx in index_list])
