efc1ed768031af11bce6fb79b89bc3fc002c94c0
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-06-15 19:42:22 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -1,9 +1,2 @@
 def solution(num_list):
-    s1 = 0
-    s2 = 0
-    for i, n in enumerate(num_list):
-        if i % 2:
-            s1 += n
-        else:
-            s2 += n
-    return max(s1, s2)
+    return max(sum(num_list[::2]), sum(num_list[1::2]))
