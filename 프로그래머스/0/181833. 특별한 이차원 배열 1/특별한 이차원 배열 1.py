8c2f9356e05470fdfe5a40aa3342d38b7b916d82
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-06-15 20:03:02 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -1,11 +1,5 @@
 def solution(n):
-    answer = []
+    answer = [[0] * n for i in range(n)]
     for i in range(n):
-        tmp = []
-        for j in range(n):
-            if i == j:
-                tmp.append(1)
-            else:
-                tmp.append(0)
-        answer.append(tmp)
+        answer[i][i] = 1
     return answer
