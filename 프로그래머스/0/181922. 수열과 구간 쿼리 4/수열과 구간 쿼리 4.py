dc604c20a821112016c141d69f0233da32cb0eb3
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-06-16 17:57:21 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 5.18 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
@@ -1,6 +1,8 @@
 def solution(arr, queries):
     for [s, e, k] in queries:
+        if k == 0 :
+            return arr
         for i in range(s, e+1):
-            if not i % k:
+            if k != 0 and not i % k:
                 arr[i] += 1
     return arr
