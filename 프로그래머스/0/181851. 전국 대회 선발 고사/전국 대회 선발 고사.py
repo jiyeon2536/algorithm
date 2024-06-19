524da96143bde53dfb24ea00141db28b07156930
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-06-19 18:48:19 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,13 +1,3 @@
 def solution(rank, attendance):
-    # 새 배열을 돌면서 어탠이 트루인거 세개를 뽑고
-    # 계산
-    c = []
-    arr = sorted([[r, i] for i, r in enumerate(rank)])
-    
-    for [_, i] in arr:
-        if len(c) == 3:
-            break
-        if attendance[i]:
-            c.append(i)
-            
-    return 10000 * c[0] + 100 * c[1] + c[2]
+    arr = sorted([[r, i] for i, r in enumerate(rank) if attendance[i]])
+    return 10000 * arr[0][1] + 100 * arr[1][1] + arr[2][1]
