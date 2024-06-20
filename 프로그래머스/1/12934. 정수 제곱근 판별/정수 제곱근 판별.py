d42bb29b31f23576f6ab0c8af3c4300fdb31fced
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-06-20 16:24:40 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,5 +1,5 @@
 def solution(n):
-    for i in range(1, n + 1):
-        if i ** 2 == n:
-            return (i + 1) ** 2
+    sqrt = n ** (1/2)
+    if sqrt % 1 == 0:
+        return (sqrt + 1) ** 2
     return -1
