2fec5a596638d8ac049b0eef0109f383b847071b
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-06-23 00:18:44 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,17 @@
+def solution(s):
+    key = {
+        'zero': 0,
+        'one' : 1,
+	    'two' : 2,	
+        'three' : 3,
+	    'four' : 4,
+	    'five' : 5,
+	    'six' : 6,
+	    'seven' : 7, 
+	    'eight': 8,
+	    'nine' : 9,
+    }
+    for k in key:
+        if k in s:
+            s = s.replace(k, str(key[k]))
+    return int(s)
