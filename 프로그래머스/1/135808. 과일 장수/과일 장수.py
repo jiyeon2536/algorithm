77070df0053bea92264aaa85c376555fbdc56c4c
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-06-26 15:30:29 +0900

    [level 1] Title: 과일 장수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,8 +1,2 @@
 def solution(k, m, score):
-    answer = 0
-    # 마지막 인덱스는 사과의 전체개수에서 나머지를 뺀만큼
-    last = len(score) - (len(score) % m)
-    score.sort(reverse=True)
-    for s in range(0, last, m):
-        answer += min(score[s: s+m]) * m
-    return answer
+    return sum(sorted(score)[len(score) % m::m] * m)
