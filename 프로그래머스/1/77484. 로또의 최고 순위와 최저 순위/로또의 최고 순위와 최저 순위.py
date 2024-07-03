791e108eacb68e76cbd89a4efce72eaa26ccff90
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-07-03 17:24:09 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,18 +1,13 @@
 def solution(lottos, win_nums):
     answer = [0, 0]
-
-    win_dict = {}
-    for w in win_nums:
-        win_dict[w] = 0
+    cor = 0
     
-    unknown = 0
     for l in lottos:
-        if l in win_dict:
-            win_dict[l] = 1
-        if l == 0:
-            unknown += 1
+        if l in win_nums:
+            cor += 1
+        answer[1] = min(7 - cor, 6)
     
-    answer[1] = min(7 - sum(win_dict.values()), 6)
-    answer[0] = min(7 - (sum(win_dict.values()) + unknown), 6)
+    cor += lottos.count(0)
+    answer[0] = min(7 - cor, 6)
     
     return answer
