8fbdb1c1416c1d1cc1ba6447c2461948d3e00e46
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-07-15 20:07:46 +0900

    [level 1] Title: 신고 결과 받기, Time: 1542.13 ms, Memory: 39.7 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,17 +1,12 @@
 def solution(id_list, report, k):
-    answer = {i:0 for i in id_list} # 몇번 연락받는지
-    reported = {i:0 for i in id_list}  # 몇번 신고당했는지
-    notify = {i:[] for i in id_list}   # 누가 이사람을 신고했는지
-    
-    for r in report:
-        reporter, reportee = r.split(' ')
-        if reporter not in notify[reportee]:
-            notify[reportee].append(reporter)
-            reported[reportee] += 1
-    
-    for p in reported:
-        if reported[p] >= k:
-            for n in notify[p]:
-                answer[n] += 1
-    
-    return list(answer.values())
+    answer = [0] * len(id_list)    
+    reports = {x : 0 for x in id_list}
+
+    for r in set(report):
+        reports[r.split()[1]] += 1
+
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
+
+    return answer
