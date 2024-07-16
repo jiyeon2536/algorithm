f4e167ebe679219185bd6b921070df6bf2d38f78
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-07-16 17:45:53 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 114 ms, Memory: 45,776 KB -BaekjoonHub

---
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -0,0 +1,17 @@
+for _ in range(10):
+    tc = input()
+    q = list(map(int, input().split()))
+
+    i = 0
+    while True:
+        tmp = q.pop(0) - (i + 1)
+        if tmp <= 0:
+            q.append(0)
+            break
+        q.append(tmp)
+        i = (i + 1) % 5
+    
+    print(f'#{tc}', end=' ')
+    for ans in q:
+        print(ans, end=' ')
+    print('')
