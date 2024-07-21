cd8d1e744e90bd36efd4003e6d4bb33d2a613521
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-07-21 15:51:04 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.44 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -0,0 +1,36 @@
+def solution(numbers, hand):
+    answer = ''    
+    l_fing = 10
+    r_fing = 12
+    
+    # dif = [[1, 3], [2, 4, 6], [5, 7, 9], [8, 10]]
+    dif = {0: 0, 1: 1, 2: 2, 3: 1, 4: 2, 5: 3, 6: 2, 7: 3, 8: 4, 9: 3, 10: 4}
+    for n in numbers:
+        ld = 0
+        rd = 0
+        if n in [1, 4, 7]:
+            answer += 'L'
+            l_fing = n
+        elif n in [3, 6, 9]:
+            answer += 'R'
+            r_fing = n
+        else:
+            if n == 0:
+                n = 11
+            l_d = dif[abs(l_fing - n)]
+            r_d = dif[abs(r_fing - n)]
+            if l_d < r_d:
+                answer += 'L'
+                l_fing = n
+            elif l_d > r_d:
+                answer += 'R'
+                r_fing = n
+            else:
+                if hand[0] == 'r':
+                    answer += 'R'
+                    r_fing = n
+                else:
+                    answer += 'L'
+                    l_fing = n
+            
+    return answer
