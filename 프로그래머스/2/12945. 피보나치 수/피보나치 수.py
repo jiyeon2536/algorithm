a8d628c4cf0f34425f0cfb71383e3263ae25b738
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-07-22 16:36:41 +0900

    [level 2] Title: 피보나치 수, Time: 90.57 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,12 +1,8 @@
-def solution(n):
-    answer = 0
-    
+def solution(n):    
     i = 1
     f_0, f_1 = 0, 1
     while i < n:
-        answer = f_0 + f_1
-        f_0 = f_1
-        f_1 = answer
+        f_0, f_1 = f_1, f_0 + f_1
         i += 1
         
-    return answer % 1234567
+    return f_1 % 1234567
