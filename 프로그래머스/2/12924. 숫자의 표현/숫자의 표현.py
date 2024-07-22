0c05f722e6e7e0be5158d049e3d021ac8a981b6d
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-07-22 16:01:44 +0900

    [level 2] Title: 숫자의 표현, Time: 6.55 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,22 +1,13 @@
-def solution(n):
+def solution(num):
     answer = 0
-    
-    arr = [i + 1 for i in range(n)]
-    start, end = 0, 0
-    total = 1
-    
-    while start <= end and end < len(arr):
-        if total == n:
-            answer += 1
-            total -= arr[start]
-            start += 1
-            
-        elif total < n:
-            end += 1
-            total += arr[end]
-            
-        elif total > n:
-            total -= arr[start]
-            start += 1
-    
+    for x in range(1,num+1):
+        sum = 0
+        for y in range(x, num+1):
+            sum += y
+            if sum == num:
+                answer += 1
+                break
+            elif sum > num:
+                break
+
     return answer
