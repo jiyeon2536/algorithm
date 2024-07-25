b08282bfe36cb91e365136729ad1d02e420024b8
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-07-25 16:42:29 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,8 +1,4 @@
 function solution(s) {
-    let answer = '';
     const arr = s.split(' ').map(Number)
-    const min = Math.min(...arr)
-    const max = Math.max(...arr)
-    answer = `${min} ${max}`
-    return answer;
+    return `${Math.min(...arr)} ${Math.max(...arr)}`
 }
