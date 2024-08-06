af9a3c582263c7b2236bff89a08735037944f733
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-08-06 14:43:15 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,12 +1,3 @@
-const intToTern = (num) => {
-    let result = '';
-    while (num >= 3) {
-        result += String(num % 3)
-        num = Math.floor(num / 3)
-    }
-    return result + num
-}
-
 const solution = (n) => {
-    return parseInt(intToTern(n), 3)
+    return parseInt([...n.toString(3)].reverse().join(''), 3)
 }
