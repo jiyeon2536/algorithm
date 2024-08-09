72859a10358bd79be079e09746311ba689ff7a00
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-08-09 15:48:20 +0900

    [level 1] Title: 시저 암호, Time: 6.36 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/README.md
+++ 프로그래머스/1/12926. 시저 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 시저 암호 - 12926 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926) 
 
 ### 성능 요약
 
-메모리: 38.1 MB, 시간: 7.79 ms
+메모리: 36.7 MB, 시간: 6.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 09일 15:43:47
+2024년 08월 09일 15:48:18
 
 ### 문제 설명
 
--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,17 +1,18 @@
-const isUpper = (ch) => {
-    if ('A'.codePointAt(0) <= ch.charCodeAt(0) && ch.charCodeAt(0) <= 'Z'.codePointAt(0)) return true
+const upperA = 'A'.codePointAt(0)
+const lowerA = 'a'.codePointAt(0)
+
+const isUpper = (asc) => {
+    if (upperA <= asc && asc <= 'Z'.codePointAt(0)) return true
     return false
 }
 
 const solution = (s, n) => {
     let result = '';
-    const upperA = 'A'.codePointAt(0)
-    const lowerA = 'a'.codePointAt(0)
-    
+
     for (let i = 0; i < s.length; i++) {
         const asc = s.charCodeAt(i)
         if (s[i] === ' ') result += s[i]
-        else if (isUpper(s[i])) {
+        else if (isUpper(asc)) {
             result += String.fromCharCode((asc + n - upperA) % 26 + upperA)
         }
         else {
