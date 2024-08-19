f225c92ee880e022759c2c31df73515331a5b902
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-08-19 16:36:46 +0900

    [level 2] Title: 피로도, Time: 69.58 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,15 +1,20 @@
-from itertools import permutations
 def solution(k, dungeons):
-    answer = -1
-    perm = list(permutations(dungeons))
     
-    for p in perm:
-        remain = k
-        tmp_ans = 0
-        for [mn, rq] in p:
-            if remain >= mn:
-                tmp_ans += 1
-                remain -= rq
-        answer = max(tmp_ans, answer)
+    def back(remain, cnt):
+        nonlocal n, answer, visited
+        if cnt >= answer:
+            answer = cnt
+        
+        for i in range(n):
+            if not visited[i] and remain >= dungeons[i][0]:
+                visited[i] = 1
+                back(remain - dungeons[i][1], cnt + 1)
+                visited[i] = 0
+    
+    
+    n = len(dungeons)
+    visited = [0] * n
+    answer = -1
+    back(k, 0)
             
     return answer
