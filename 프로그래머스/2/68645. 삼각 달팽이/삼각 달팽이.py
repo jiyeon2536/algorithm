b428ea4b00dff176b56b9d883050cfb804f0b711
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-09-13 16:45:26 +0900

    [level 2] Title: 삼각 달팽이, Time: 292.03 ms, Memory: 43.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -0,0 +1,28 @@
+di = [1, 0, -1, 0]
+dj = [0, 1, -1, -1]
+
+def solution(n):
+    answer = [[0] * (_+1) for _ in range(n)]
+    total = sum([i + 1 for i in range(n)])
+    cnt = 1
+    
+    now_i, now_j = 0, 0
+    answer[now_i][now_j] = 1
+    dir_idx = 0
+
+    while cnt < total:
+        nxt_i, nxt_j = now_i + di[dir_idx], now_j + dj[dir_idx]
+        
+        if 0 <= nxt_i < n and 0 <= nxt_j <= nxt_i and answer[nxt_i][nxt_j] == 0:
+            cnt += 1 
+            answer[nxt_i][nxt_j] = cnt
+            now_i, now_j = nxt_i, nxt_j
+        else:
+            dir_idx = (dir_idx + 1) % 4
+    
+    res = []
+    for a in answer:
+        for b in a:
+            res.append(b)
+    
+    return res
